1d478d15c4adfa1df8f46ede11a193ba3d0972f3
Nikola Kostov <nikola_kostov92@mail.bg>
2013-02-17 07:22:05 +0200

    Commited exam assignments

---
+++ Telerik Academy Exam 2 @ 6 Feb 2012/3DStars/3DStars.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DStars
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstLine = Console.ReadLine();
+            string[] firsts = firstLine.Split(' ');
+            int[] args = new int[firsts.Length];
+
+            for (int i = 0; i < firsts.Length; i++)
+            {
+                args[i] = int.Parse(firsts[i]);
+            }
+            //int[, ,] arr = new int[4, 2, 3];
+            char[, ,] grid = new char[args[2], args[1], args[0]];
+            for (int i = 0; i < args[1]; i++)
+            {
+                string[] rows = Console.ReadLine().Split(' ');
+                for (int j = 0; j < rows.Length; j++)
+                {
+                    for (int k = 0; k < rows[j].Length; k++)
+                    {
+                        grid[j, i, k] = rows[j][k];
+                    }
+                }
+            }
+
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            int sum = 0;
+            for (int i = 1; i < args[2] - 1; i++)
+            {
+                for (int j = 1; j < args[1] - 1; j++)
+                {
+                    for (int k = 1; k < args[0] - 1; k++)
+                    {
+                        char color = grid[i, j, k];
+                        if (grid[i - 1, j, k] == color && grid[i + 1, j, k] == color && grid[i, j - 1, k] == color && grid[i, j, k + 1] == color && grid[i, j + 1, k] == color && grid[i, j, k - 1] == color)
+                        {
+                            if (dic.ContainsKey(color))
+                            {
+                                int value;
+                                dic.TryGetValue(color,out value);
+                                dic[color] = value + 1;
+                                sum++;
+                            } 
+                            else
+                            {
+                                dic.Add(color, 1);
+                                sum++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+            foreach (var item in dic.OrderBy(key => key.Key))
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+
+
+    }
+}
---
+++ Telerik Academy Exam 2 @ 6 Feb 2012/Tubes/Tubes.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Tubs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            long sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += array[i];
+            }
+            int size = (int)(sum / m);
+            if (size == 0)
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                count += array[i] / size;
+            }
+            if (count == m)
+            {
+                Console.WriteLine(size);
+                return;
+            } 
+            else
+            {
+                int down = 0;
+                int up = int.MaxValue;
+                do 
+                {
+                    count = 0;
+                    int middle = (down + up) / 2;
+                    for (int i = 0; i < n; i++)
+                    {
+                        count += array[i] / middle;
+                    }
+                    if (count >= m)
+                    {
+                        down = middle + 1;
+                        size = middle;
+                    } 
+                    else
+                    {
+                        up = middle - 1;
+                    }
+                } while (down <= up);
+            }
+
+            //int k = 0;
+            //bool up = true;
+            //do
+            //{
+            //    if (size == 0)
+            //    {
+            //        break;
+            //    }
+            //    count = 0;
+            //    for (int i = 0; i < n; i++)
+            //    {
+            //        count += array[i] / size;
+            //    }
+            //    if (count == m)
+            //    {
+            //        if (up)
+            //        {
+            //            do
+            //            {
+            //                size++;
+            //                count = 0;
+            //                for (int i = 0; i < n; i++)
+            //                {
+            //                    count += array[i] / size;
+            //                }
+            //            } while (count == m);
+            //            size--;
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        int s = 0;
+            //        for (int i = 0; i <= k; i++)
+            //        {
+            //            s += array[i] / size;
+            //        }
+            //        int s1 = 0;
+            //        for (int i = k + 1; i < n; i++)
+            //        {
+            //            s1 += array[i];
+            //        }
+            //        if (size > (int)Math.Ceiling(s1 / (float)(m - s)))
+            //        {
+            //            size = (int)Math.Ceiling(s1 / (float)(m - s));
+            //        }
+            //        if (k < n - 1)
+            //        {
+            //            k++;
+            //        }
+            //    }
+            //} while (count != m);
+
+            Console.WriteLine(size);
+        }
+    }
+}
