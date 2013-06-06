527d1d84afd51415a505447c84030b87fcddb9d7
Nikola Kostov <nikola_kostov92@mail.bg>
2013-06-07 01:59:07 +0300

    Added Advanced Data Structures - Priority Queue
    Priority Queue Implementation

---
+++ DSA/AdvancedDataStructures/1. PriorityQueue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1.PriorityQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PriorityQueue<int> heap = new PriorityQueue<int>(2);
+            heap.Enqueue(1);
+            heap.Enqueue(2);
+            heap.Enqueue(3);
+            heap.Enqueue(4);
+            heap.Dequeue();
+            heap.Enqueue(3);
+            heap.Enqueue(5);
+            Console.WriteLine(heap.Dequeue());
+            heap.Print();
+            heap.Enqueue(2);
+            heap.Print();
+        }
+    }
+}
