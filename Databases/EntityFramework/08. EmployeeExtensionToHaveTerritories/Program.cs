347bb08296bc308ac1c046bc45983324b27a511f
Nikola Kostov <nikola_kostov92@mail.bg>
2013-07-17 22:06:21 +0300

    Fixed EmployeeExtension to be in the same assembly as the other partial class. Added demo using the new functionality of Employee.

---
+++ Databases/EntityFramework/08. EmployeeExtensionToHaveTerritories/Program.cs
@@ -0,0 +1,23 @@
+﻿using _01.UsingEntityFrameworkModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.EmployeeExtensionToHaveTerritories
+{
+    class Program
+    {
+        public static void Main()
+        {
+            NorthwindEntities context = new NorthwindEntities();
+            Employee employee = context.Employees.First();
+            foreach (var territory in employee.EntityTerritories)
+            {
+                Console.WriteLine(territory.TerritoryDescription);
+            }
+            
+        }
+    }
+}
